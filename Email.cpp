a07d37b7e2d17c5b5ac05d312fbe813511f26c37
RefanKa <39189874+RefanKa@users.noreply.github.com>
2018-05-11 14:39:32 +0000

    Update Email.cpp

--- Email.cpp
+++ Email.cpp
@@ -4,4 +4,21 @@
 
 Using namespace std;
 
-Class Pesan{
+Class Email{
+  protected:
+  string IsiPesan;
+  string Pengirim;
+  
+  public:
+  Email(string IsiPesan, string Pengirim){
+  this->IsiPesan = IsiPesan;
+  this->Pengirim = Pengirim;
+   }
+};
+class Gmail : public Email
+  private:
+  string AlamatEmail;
+  public:
+  Gmail(string IsiPesan, string AlamatEmail):Gmail(IsiPesan, 0){
+  this->AlamatEmail= AlamatEmail;
+  this->Pengirim = Pengirim;
